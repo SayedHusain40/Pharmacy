060aab6532c2ac242324fd38ff28a01f16e9df59
Fatima Ebrahim Ali Abdulla <83508576+fa1819@users.noreply.github.com>
2023-12-03 05:10:12 +0300

    U

--- Reports/Report.php
+++ Reports/Report.php
@@ -1,10 +1,10 @@
-<a class="dropdown-item" href="../.php">Medicine Report</a>
-          <a class="dropdown-item" href="../.php">Current Inventory Report</a>
-          <a class="dropdown-item" href="../.php">Users Report</a>
-          <a class="dropdown-item" href="../.php">Customer Information Report</a>
-          <a class="dropdown-item" href="../.php">Staff Information Report</a>
-          <a class="dropdown-item" href="../.php">Customer Order Details Report</a>
-          <a class="dropdown-item" href="../.php">Top Selling Medicines Report</a>
-          <a class="dropdown-item" href="../.php">Supplier Report</a>
-          <a class="dropdown-item" href="../.php">Order Report</a>
-          <a class="dropdown-item" href="../.php">Offers Report</a>
+          <a class="dropdown-item" href="../.php"> Medicine Report </a>
+          <a class="dropdown-item" href="../.php"> Current Inventory Report </a>
+          <a class="dropdown-item" href="../.php"> Users Report </a>
+          <a class="dropdown-item" href="../.php"> Customer Information Report </a>
+          <a class="dropdown-item" href="../.php"> Staff Information Report </a>
+          <a class="dropdown-item" href="../.php"> Customer Order Details Report </a>
+          <a class="dropdown-item" href="../.php"> Top Selling Medicines Report </a>
+          <a class="dropdown-item" href="../.php"> Supplier Report </a>
+          <a class="dropdown-item" href="../.php"> Order Report </a>
+          <a class="dropdown-item" href="../.php"> Offers Report </a>
