903e37366724c982889243ee0b20c42e9eb4fe32
s.husain <s.husain_10@hotmail.com>
2023-12-08 02:12:15 +0300

    t

--- Interface/PaymentPage.php
+++ Interface/PaymentPage.php
@@ -45,22 +45,71 @@ try {
       $updateQty->execute([$newQty, $row["ProductID"]]);
     }
 
-    // Insert Payment into the (payment database table)
+
+    //Get CreditCardInfo to store them in database
+    $cardholderName = $_POST['cardholder-name'];
+    $cardNumber = $_POST['card-number'];
+    $expirationDate = $_POST['expiration-date'];
+    $cvv = $_POST['cvv'];
+
+    // Encrypt sensitive data
+    $key = 'secretKey';
+    $iv = openssl_random_pseudo_bytes(16);
+    $encryptedCardNumber = openssl_encrypt($cardNumber, 'AES-256-CBC', $key, 0, $iv);
+
+    $CreditCardInfo = [
+      'cardholderName' => $cardholderName,
+      'encryptedCardNumber' => $encryptedCardNumber,
+      'expirationDate' => $expirationDate,
+      'cvv' => $cvv,
+    ];
+
+    $CardInfoArray = json_encode($CreditCardInfo);
+
+
+    // Insert PaymentInfo into the (payment database table)
     date_default_timezone_set('Asia/Bahrain');
-    $PayDate = date('Y-m-d'); 
+    $PayDate = date('Y-m-d');
     $totalPrice = $_SESSION['TotalPrice'];
-    $stmtPayment = $db->prepare("INSERT INTO `payment database` (PayDate, Total, Details, UserID) VALUES (?, ?, ?, ?)");
-    $stmtPayment->execute([$PayDate, $totalPrice, $totalPrice, $userID]);
+    $PaymentStatus = "paid";
+
+
+    // customer data for shippingInfo
+    $stmt = $db->prepare("SELECT * FROM `customer data` WHERE UserID = ?");
+    $stmt->execute([$userID]);
+    $result = $stmt->fetch();
+
+    $shippingInfo = [
+      'RecipientName' => $result['FirstName'] . ' ' . $result['LastName'],
+      'Address' => [
+        'Area' => $result['Area'],
+        'House' => $result['House'],
+        'Street' => $result['Street'],
+        'Block' => $result['Block'],
+      ],
+      'Contact' => [
+        'MobileNumber' => $result['MobileNumber'],
+        'Email' => $result['Email'],
+      ]
+    ];
+
+    $shippingInfoArray = json_encode($shippingInfo);
+
+
+    $stmtPayment = $db->prepare("INSERT INTO `payment database` (PayDate, Total, ShippingInfo, CreditCardInfo, PaymentStatus, UserID) VALUES (?,?, ?, ?, ?, ?)");
+    $stmtPayment->execute([$PayDate, $totalPrice, $shippingInfoArray, $CardInfoArray, $PaymentStatus, $userID]);
 
     // Get the last inserted PaymentID
     $PaymentID = $db->lastInsertId();
 
-    // Update PaymentID in (order data table)
-    $updatePaymentID = $db->prepare("UPDATE `order data` SET PaymentID = ? WHERE OrderID = ?");
-    $updatePaymentID->execute([$PaymentID, $OrderID]);
+
+    // Update (order data table)
+    $Status = "Payment Confirmed";
+    $update = $db->prepare("UPDATE `order data` SET PaymentID = ?, Status = ?, CreditCardInfo = ? WHERE OrderID = ?");
+    $update->execute([$PaymentID, $Status, $CardInfoArray, $OrderID]);
+
 
     echo "Thank You for Your Purchase";
-
     exit();
   } else {
 
@@ -77,12 +126,13 @@ try {
       $stmt = $db->prepare("SELECT * FROM `order data` WHERE UserID = ? AND PaymentID IS NULL");
       $stmt->execute([$userID]);
       $count = $stmt->rowCount();
+      $Status = "Payment Pending";
 
       //check if Order Exit Or no if not insert order else means there is order but not pay so will update it
       if ($count == 0) {
-        $sql = "INSERT INTO `order data` (UserID, TotalPrice, PaymentMethod, OrderDate) VALUES (?, ?, ?, ?)";
+        $sql = "INSERT INTO `order data` (UserID, TotalPrice, PaymentMethod, Status,  OrderDate) VALUES (?, ?, ?, ?, ?)";
         $data = $db->prepare($sql);
-        $data->execute([$userID, $totalPrice, $paymentMethod, $OrderDate]);
+        $data->execute([$userID, $totalPrice, $paymentMethod, $Status, $OrderDate]);
       } else {
         $sql = "UPDATE `order data` SET TotalPrice = ?, PaymentMethod = ?, OrderDate = ?";
         $data = $db->prepare($sql);
@@ -132,13 +182,13 @@ try {
 
           <form class="paymentForm" method="post">
             <label>Cardholder Name (exactly as shown on card)</label>
-            <input type="text" placeholder="Enter Your Full Name" id="cardholder-name" />
+            <input type="text" placeholder="Enter Your Full Name" id="cardholder-name" name="cardholder-name" />
             <p class="name-error MainError"><i class="fa-solid fa-circle-exclamation"></i><span>Cardholder Name cannot be empty</span></p>
 
             <br /> <br>
 
             <label>Card number</label>
-            <input type="text" placeholder="0000 0000 0000 0000" id="card-number" />
+            <input type="text" placeholder="0000 0000 0000 0000" id="card-number" name="card-number" />
             <p class="card-error MainError"><i class="fa-solid fa-circle-exclamation"></i><span>Card number cannot be empty</span></p>
 
             <br /><br>
@@ -146,14 +196,14 @@ try {
             <div class="parentDiv">
               <div>
                 <label>Expiration date</label><br />
-                <input type="text" placeholder="MM/YY" class="ExpDate" id="expiration-date" />
+                <input type="text" placeholder="MM/YY" class="ExpDate" id="expiration-date" name="expiration-date" />
                 <p class="expiration-error MainError">
                   <i class="fa-solid fa-circle-exclamation"></i><span>Expiration date cannot be empty</span>
                 </p>
               </div>
               <div>
                 <label>CVV</label><br />
-                <input type="text" placeholder="CVV" id="cvv" />
+                <input type="text" placeholder="CVV" id="cvv" name="cvv"/>
                 <p class="cvv-error MainError">
                   <i class="fa-solid fa-circle-exclamation"></i><span>CVV cannot be empty</span>
                 </p>
