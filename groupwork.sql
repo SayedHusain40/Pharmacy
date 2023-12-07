5dbc0199cc0df3fe99e79e9fd981bb166ae98beb
Fatima Ebrahim Ali Abdulla <83508576+fa1819@users.noreply.github.com>
2023-12-07 15:06:19 +0300

    U

--- groupwork.sql
+++ groupwork.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2023 at 06:21 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Dec 07, 2023 at 01:05 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,13 +29,26 @@ USE `groupwork`;
 -- Table structure for table `account`
 --
 
+DROP TABLE IF EXISTS `account`;
 CREATE TABLE IF NOT EXISTS `account` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_name` varchar(255) DEFAULT NULL,
   `member_id` int(11) DEFAULT NULL,
+  `task_status` enum('Done','NotDone') DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `member_id` (`member_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`id`, `task_name`, `member_id`, `task_status`) VALUES
+(1, 'Profile.php\r\n', 3, 'NotDone'),
+(2, 'EditProfile.php\r\n', 2, 'NotDone'),
+(3, 'Signup.php\r\n', 1, 'Done'),
+(4, 'Login.php\r\n', 1, 'Done'),
+(5, 'Logout.php', 1, 'Done');
 
 -- --------------------------------------------------------
 
@@ -43,13 +56,27 @@ CREATE TABLE IF NOT EXISTS `account` (
 -- Table structure for table `function`
 --
 
+DROP TABLE IF EXISTS `function`;
 CREATE TABLE IF NOT EXISTS `function` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_name` varchar(255) DEFAULT NULL,
   `member_id` int(11) DEFAULT NULL,
+  `task_status` enum('Done','NotDone') DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `member_id` (`member_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `function`
+--
+
+INSERT INTO `function` (`id`, `task_name`, `member_id`, `task_status`) VALUES
+(1, 'PayForOrder.php', 1, 'NotDone'),
+(2, 'SearchItems', 3, 'NotDone'),
+(3, 'Trackorder.php', 1, 'NotDone'),
+(4, 'Rating.php', 1, 'NotDone'),
+(5, 'Sorting.php', 1, 'NotDone'),
+(6, 'PriceFilter.php', 3, 'NotDone');
 
 -- --------------------------------------------------------
 
@@ -57,13 +84,39 @@ CREATE TABLE IF NOT EXISTS `function` (
 -- Table structure for table `interface`
 --
 
+DROP TABLE IF EXISTS `interface`;
 CREATE TABLE IF NOT EXISTS `interface` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_name` varchar(255) DEFAULT NULL,
   `member_id` int(11) DEFAULT NULL,
+  `task_status` enum('Done','NotDone') DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `member_id` (`member_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `interface`
+--
+
+INSERT INTO `interface` (`id`, `task_name`, `member_id`, `task_status`) VALUES
+(1, 'Header.php', 4, 'NotDone'),
+(2, 'Footer.php', 4, 'NotDone'),
+(3, 'HomePage.php', 4, 'NotDone'),
+(4, 'HomePageCustomer.php', 4, 'NotDone'),
+(5, 'HomePageStaff.php', 4, 'NotDone'),
+(6, 'HomePageAdmin.php', 4, 'NotDone'),
+(7, 'HomePageSupplier.php', 1, 'NotDone'),
+(8, 'ShopByCategories.php', 1, 'NotDone'),
+(9, 'ShopByBrand.php', 1, 'NotDone'),
+(10, 'ShoppingCart.php', 1, 'NotDone'),
+(11, 'AddToCart&Favourite.php', 1, 'NotDone'),
+(12, 'ViewCart.php', 1, 'NotDone'),
+(13, 'WishListPage.php', 1, 'NotDone'),
+(14, 'PaymentPage.php', 1, 'NotDone'),
+(15, 'Checkout.php', 1, 'NotDone'),
+(16, 'AboutUs.php', 1, 'NotDone'),
+(17, 'ContactUs.php', 1, 'NotDone'),
+(18, 'FAQs.php', 1, 'NotDone');
 
 -- --------------------------------------------------------
 
@@ -71,13 +124,24 @@ CREATE TABLE IF NOT EXISTS `interface` (
 -- Table structure for table `manageoffers`
 --
 
+DROP TABLE IF EXISTS `manageoffers`;
 CREATE TABLE IF NOT EXISTS `manageoffers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_name` varchar(255) DEFAULT NULL,
   `member_id` int(11) DEFAULT NULL,
+  `task_status` enum('Done','NotDone') DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `member_id` (`member_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `manageoffers`
+--
+
+INSERT INTO `manageoffers` (`id`, `task_name`, `member_id`, `task_status`) VALUES
+(1, 'AddOffer.php', 3, 'NotDone'),
+(2, 'EditOffer.php', 3, 'NotDone'),
+(3, 'ViewOffer.php', 3, 'NotDone');
 
 -- --------------------------------------------------------
 
@@ -85,13 +149,24 @@ CREATE TABLE IF NOT EXISTS `manageoffers` (
 -- Table structure for table `manageorders`
 --
 
+DROP TABLE IF EXISTS `manageorders`;
 CREATE TABLE IF NOT EXISTS `manageorders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_name` varchar(255) DEFAULT NULL,
   `member_id` int(11) DEFAULT NULL,
+  `task_status` enum('Done','NotDone') DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `member_id` (`member_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `manageorders`
+--
+
+INSERT INTO `manageorders` (`id`, `task_name`, `member_id`, `task_status`) VALUES
+(1, 'AddOrder.php', 1, 'NotDone'),
+(2, 'EditOrder.php', 1, 'NotDone'),
+(3, 'ViewOrderList.php', 1, 'NotDone');
 
 -- --------------------------------------------------------
 
@@ -99,13 +174,25 @@ CREATE TABLE IF NOT EXISTS `manageorders` (
 -- Table structure for table `manageproducts`
 --
 
+DROP TABLE IF EXISTS `manageproducts`;
 CREATE TABLE IF NOT EXISTS `manageproducts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_name` varchar(255) DEFAULT NULL,
   `member_id` int(11) DEFAULT NULL,
+  `task_status` enum('Done','NotDone') DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `member_id` (`member_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `manageproducts`
+--
+
+INSERT INTO `manageproducts` (`id`, `task_name`, `member_id`, `task_status`) VALUES
+(1, 'AddProduct.php', 1, 'NotDone'),
+(2, 'EditProduct.php', 1, 'NotDone'),
+(3, 'ViewProductList.php', 1, 'NotDone'),
+(4, 'ProductDetails.php', 1, 'NotDone');
 
 -- --------------------------------------------------------
 
@@ -113,13 +200,30 @@ CREATE TABLE IF NOT EXISTS `manageproducts` (
 -- Table structure for table `manageusers`
 --
 
+DROP TABLE IF EXISTS `manageusers`;
 CREATE TABLE IF NOT EXISTS `manageusers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_name` varchar(255) DEFAULT NULL,
   `member_id` int(11) DEFAULT NULL,
+  `task_status` enum('Done','NotDone') DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `member_id` (`member_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `manageusers`
+--
+
+INSERT INTO `manageusers` (`id`, `task_name`, `member_id`, `task_status`) VALUES
+(1, 'AddUsers.php', 1, 'NotDone'),
+(2, 'EditUsers.php', 1, 'NotDone'),
+(3, 'ViewUsers.php', 1, 'NotDone'),
+(4, 'AddCustomer.php', 1, 'NotDone'),
+(5, 'EditCustomer.php', 1, 'NotDone'),
+(6, 'AddStaff.php', 1, 'NotDone'),
+(7, 'EditStaff.php', 1, 'NotDone'),
+(8, 'AddSupplier.php', 1, 'NotDone'),
+(9, 'EditSupplier.php', 1, 'NotDone');
 
 -- --------------------------------------------------------
 
@@ -127,6 +231,7 @@ CREATE TABLE IF NOT EXISTS `manageusers` (
 -- Table structure for table `member`
 --
 
+DROP TABLE IF EXISTS `member`;
 CREATE TABLE IF NOT EXISTS `member` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `name` enum('Fatema','Elias','Sayed Hussian','Karrar') DEFAULT NULL,
@@ -149,13 +254,31 @@ INSERT INTO `member` (`Id`, `name`) VALUES
 -- Table structure for table `reports`
 --
 
+DROP TABLE IF EXISTS `reports`;
 CREATE TABLE IF NOT EXISTS `reports` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_name` varchar(255) DEFAULT NULL,
   `member_id` int(11) DEFAULT NULL,
+  `task_status` enum('Done','NotDone') DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `member_id` (`member_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reports`
+--
+
+INSERT INTO `reports` (`id`, `task_name`, `member_id`, `task_status`) VALUES
+(1, 'CurrentInventoryReport.php', 1, 'NotDone'),
+(2, 'MedicineReport.php', 1, 'NotDone'),
+(3, 'UsersReport.php', 1, 'NotDone'),
+(4, 'CustomerInformationReport.php', 1, 'NotDone'),
+(5, 'StaffInformationReport.php', 1, 'NotDone'),
+(6, 'CustomerOrderDetailsReport.php', 1, 'NotDone'),
+(7, 'TopSellingMedicinesReport.php', 1, 'NotDone'),
+(8, 'SupplierReport.php', 1, 'NotDone'),
+(9, 'OrderReport.php', 2, 'NotDone'),
+(10, 'OfferReport.php', 1, 'NotDone');
 
 --
 -- Constraints for dumped tables
