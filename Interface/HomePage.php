d1604f440ced96d9e95056354d9e00be5894fc2a
Fatima Ebrahim Ali Abdulla <83508576+fa1819@users.noreply.github.com>
2023-12-09 15:05:57 +0300

    Background

--- Interface/HomePage.php
+++ Interface/HomePage.php
@@ -7,8 +7,16 @@ include '../header.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+            background-image: url("../images/b1.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
 </head>
 <body>
+
     <!-- Header -->
     <!-- nav -->
     <!-- body -->
--- Interface/HomePageAdmin.php
+++ Interface/HomePageAdmin.php
@@ -1,3 +1,23 @@
 <?php
 session_start();
 include '../header.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-image: url("../images/b1.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
