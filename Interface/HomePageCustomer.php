754a07e85cee7e8da9271679c539afdf3667ff03
Fatima Ebrahim Ali Abdulla <83508576+fa1819@users.noreply.github.com>
2023-12-13 16:49:56 +0300

    U

--- Interface/HomePageCustomer.php
+++ Interface/HomePageCustomer.php
@@ -1,14 +1,6 @@
 <?php
-session_start();
 include '../header.php';
 ?>
-<?php
-include '../Connection/init.php';
-
-$sql = "SELECT * FROM `product data`";
-$all_product = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);  // Fetch all rows as an associative array
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,22 +18,9 @@ $all_product = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);  // Fetch all rows 
 </head>
 <body>
     <main>
-        <?php
-        foreach ($all_product as $row) {  // Iterate over each row of the result set
-        ?>
-            <div class="card">
-                <div class="image">
-                    <img src="<?php echo $row["Photo"]; ?>" alt="">
-                </div>
-                <div class="caption">
-                    <p class="product_name"><?php echo $row["Name"]; ?></p>
-                    <p class="price"><b>$<?php echo $row["Price"]; ?></b></p>
-                </div>
-                <button class="add" data-id="<?php echo $row["ProductID"]; ?>">Add to cart</button>
-            </div>
-        <?php
-        }
-        ?>
+    <?php
+       include ("../Test/addToC.php");
+      ?>
     </main>
 </body>
 </html>
