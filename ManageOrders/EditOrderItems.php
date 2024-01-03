bbbb50db6a71171ccb8e0d3df241d174492c3af9
Fatima Ebrahim Ali Abdulla <83508576+fa1819@users.noreply.github.com>
2024-01-03 20:30:03 +0300

    U

---
+++ ManageOrders/EditOrderItems.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+try {
+  require("../Connection/init.php");
+
+  //delete Items from cart when click on delete
+  if (isset($_REQUEST['cartID']) && isset($_REQUEST['delete'])) {
+
+    $cartID = $_REQUEST['cartID'];
+
+    $deleteQuery = "DELETE FROM `view cart` WHERE CartID = ?";
+    $stmt = $db->prepare($deleteQuery);
+    $stmt->execute([$cartID]);
+
+
+    $_SESSION['deleteProduct_success'] = "true";
+    header("Location:  ViewShoppingCart.php");
+    exit();
+  }
+
+
+  //Update Qty and Price When click on update QTY
+  if (isset($_POST['update-qty']) && isset($_POST['newQTY']) && isset($_POST['cartID'])) {
+
+    $productID = $_POST["productID"];
+    $sql = $db->prepare("SELECT Price FROM `product data` WHERE ProductID = ?");
+    $sql->execute([$productID]);
+
+    $ProductPrice = $sql->fetch();
+    $total = $_POST['newQTY'] * $ProductPrice["Price"];
+
+    $UpdateQTY = "UPDATE `view cart` SET Qty = ?, Total = ? WHERE `CartID` = ?";
+    $stmt = $db->prepare($UpdateQTY);
+    $stmt->execute([$_POST['newQTY'], $total, $_POST['cartID']]);
+    $count = $stmt->rowCount();
+
+    $_SESSION['updateQty_success'] = "true";
+    header("Location:  ViewShoppingCart.php");
+    exit();
+  }
+
+  $db = null;
+} catch (PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
