7f34802f3d44c70457e1cb20668ec09a6c3ddbf0
s.husain <s.husain_10@hotmail.com>
2024-01-03 20:10:00 +0300

    t

--- ManageProduct/CustomerProduct.php
+++ ManageProduct/CustomerProduct.php
@@ -6,7 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../css/main.css" />
   <link rel="stylesheet" href="../css/Pdetails.css" />
+  <link rel="stylesheet" href="../css/all.min.css" />
 
 </head>
 
